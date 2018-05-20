36e3c49d4c4bf24e3e39139a4c9fd97df8034621
2KangHo <ho7719@gmail.com>
2018-05-21 03:10:46 +0900

    Update

--- actors.py
+++ actors.py
@@ -29,30 +29,49 @@ class Actor(cocos.sprite.Sprite):
 
 class Blinky(Actor):
     def __init__(self, x, y, actions):
-        load_animation('Ghosts/Blinky.png', 1, 8, 0.25)
-        super(Blinky, self).__init__('', x, y)
+        blinky_up    = load_animation('Ghosts/blinky_up.png', 1, 2, 0.25)
+        blinky_down  = load_animation('Ghosts/blinky_down.png', 1, 2, 0.25)
+        blinky_left  = load_animation('Ghosts/blinky_left.png', 1, 2, 0.25)
+        blinky_right = load_animation('Ghosts/blinky_right.png', 1, 2, 0.25)
+        super(Blinky, self).__init__(blinky_up, x, y)
         self.do(actions)
 
 
 class Clyde(Actor):
     def __init__(self, x, y, actions):
-        super(Clyde, self).__init__('', x, y)
+        clyde_up    = load_animation('Ghosts/clyde_up.png', 1, 2, 0.25)
+        clyde_down  = load_animation('Ghosts/clyde_down.png', 1, 2, 0.25)
+        clyde_left  = load_animation('Ghosts/clyde_left.png', 1, 2, 0.25)
+        clyde_right = load_animation('Ghosts/clyde_right.png', 1, 2, 0.25)
+        super(Clyde, self).__init__(clyde_up, x, y)
         self.do(actions)
 
 
 class Inky(Actor):
     def __init__(self, x, y, actions):
-        super(Inky, self).__init__('', x, y)
+        inky_up    = load_animation('Ghosts/inky_up.png', 1, 2, 0.25)
+        inky_down  = load_animation('Ghosts/inky_down.png', 1, 2, 0.25)
+        inky_left  = load_animation('Ghosts/inky_left.png', 1, 2, 0.25)
+        inky_right = load_animation('Ghosts/inky_right.png', 1, 2, 0.25)
+        super(Inky, self).__init__(inky_up, x, y)
         self.do(actions)
 
 
 class Pinky(Actor):
     def __init__(self, x, y, actions):
-        super(Pinky, self).__init__('', x, y)
+        pinky_up    = load_animation('Ghosts/pinky_up.png', 1, 2, 0.25)
+        pinky_down  = load_animation('Ghosts/pinky_down.png', 1, 2, 0.25)
+        pinky_left  = load_animation('Ghosts/pinky_left.png', 1, 2, 0.25)
+        pinky_right = load_animation('Ghosts/pinky_right.png', 1, 2, 0.25)
+        super(Pinky, self).__init__(pinky_up, x, y)
         self.do(actions)
 
 
 class Player(Actor):
     def __init__(self, x, y, actions):
-        super(Player, self).__init__('', x, y)
+        pacman_up    = load_animation('Player/pacman_up.png', 1, 4, 0.25)
+        pacman_down  = load_animation('Player/pacman_down.png', 1, 4, 0.25)
+        pacman_left  = load_animation('Player/pacman_left.png', 1, 4, 0.25)
+        pacman_right = load_animation('Player/pacman_right.png', 1, 4, 0.25)
+        super(Player, self).__init__(pacman_up, x, y)
         self.do(actions)
