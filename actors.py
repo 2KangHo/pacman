64d36dbf62ebb382bd379931725e7d1f3af8efbf
2KangHo <ho7719@gmail.com>
2018-05-23 05:06:20 +0900

    update actors.py

--- actors.py
+++ actors.py
@@ -10,11 +10,17 @@ import pyglet.image
 from pyglet.image import Animation
 
 
-def load_animation(img, x, y, dt):
+def load_animation(img, x, y, loop):
     raw = pyglet.image.load(img)
     seq = pyglet.image.ImageGrid(raw, x, y)
-    return Animation.from_image_sequence(seq, dt, False)
+    return Animation.from_image_sequence(seq, 0.25, loop)
 
+
+class Die(cocos.sprite.Sprite):
+    def __init__(self, pos):
+        die_img = load_animation('Player/pacman_die.png', 1, 11, False)
+        super(Die, self).__init__(die_img, pos)
+        self.do(ac.Delay(1) + ac.CallFunc(self.kill))
 
 class Actor(cocos.sprite.Sprite):
     def __init__(self, img, x, y):
@@ -29,49 +35,49 @@ class Actor(cocos.sprite.Sprite):
 
 class Blinky(Actor):
     def __init__(self, x, y, actions):
-        blinky_up    = load_animation('Ghosts/blinky_up.png', 1, 2, 0.25)
-        blinky_down  = load_animation('Ghosts/blinky_down.png', 1, 2, 0.25)
-        blinky_left  = load_animation('Ghosts/blinky_left.png', 1, 2, 0.25)
-        blinky_right = load_animation('Ghosts/blinky_right.png', 1, 2, 0.25)
+        blinky_up    = load_animation('Ghosts/blinky_up.png', 1, 2, True)
+        blinky_down  = load_animation('Ghosts/blinky_down.png', 1, 2, True)
+        blinky_left  = load_animation('Ghosts/blinky_left.png', 1, 2, True)
+        blinky_right = load_animation('Ghosts/blinky_right.png', 1, 2, True)
         super(Blinky, self).__init__(blinky_up, x, y)
         self.do(actions)
 
 
 class Clyde(Actor):
     def __init__(self, x, y, actions):
-        clyde_up    = load_animation('Ghosts/clyde_up.png', 1, 2, 0.25)
-        clyde_down  = load_animation('Ghosts/clyde_down.png', 1, 2, 0.25)
-        clyde_left  = load_animation('Ghosts/clyde_left.png', 1, 2, 0.25)
-        clyde_right = load_animation('Ghosts/clyde_right.png', 1, 2, 0.25)
+        clyde_up    = load_animation('Ghosts/clyde_up.png', 1, 2, True)
+        clyde_down  = load_animation('Ghosts/clyde_down.png', 1, 2, True)
+        clyde_left  = load_animation('Ghosts/clyde_left.png', 1, 2, True)
+        clyde_right = load_animation('Ghosts/clyde_right.png', 1, 2, True)
         super(Clyde, self).__init__(clyde_up, x, y)
         self.do(actions)
 
 
 class Inky(Actor):
     def __init__(self, x, y, actions):
-        inky_up    = load_animation('Ghosts/inky_up.png', 1, 2, 0.25)
-        inky_down  = load_animation('Ghosts/inky_down.png', 1, 2, 0.25)
-        inky_left  = load_animation('Ghosts/inky_left.png', 1, 2, 0.25)
-        inky_right = load_animation('Ghosts/inky_right.png', 1, 2, 0.25)
+        inky_up    = load_animation('Ghosts/inky_up.png', 1, 2, True)
+        inky_down  = load_animation('Ghosts/inky_down.png', 1, 2, True)
+        inky_left  = load_animation('Ghosts/inky_left.png', 1, 2, True)
+        inky_right = load_animation('Ghosts/inky_right.png', 1, 2, True)
         super(Inky, self).__init__(inky_up, x, y)
         self.do(actions)
 
 
 class Pinky(Actor):
     def __init__(self, x, y, actions):
-        pinky_up    = load_animation('Ghosts/pinky_up.png', 1, 2, 0.25)
-        pinky_down  = load_animation('Ghosts/pinky_down.png', 1, 2, 0.25)
-        pinky_left  = load_animation('Ghosts/pinky_left.png', 1, 2, 0.25)
-        pinky_right = load_animation('Ghosts/pinky_right.png', 1, 2, 0.25)
+        pinky_up    = load_animation('Ghosts/pinky_up.png', 1, 2, True)
+        pinky_down  = load_animation('Ghosts/pinky_down.png', 1, 2, True)
+        pinky_left  = load_animation('Ghosts/pinky_left.png', 1, 2, True)
+        pinky_right = load_animation('Ghosts/pinky_right.png', 1, 2, True)
         super(Pinky, self).__init__(pinky_up, x, y)
         self.do(actions)
 
 
 class Player(Actor):
     def __init__(self, x, y, actions):
-        pacman_up    = load_animation('Player/pacman_up.png', 1, 4, 0.25)
-        pacman_down  = load_animation('Player/pacman_down.png', 1, 4, 0.25)
-        pacman_left  = load_animation('Player/pacman_left.png', 1, 4, 0.25)
-        pacman_right = load_animation('Player/pacman_right.png', 1, 4, 0.25)
+        pacman_up    = load_animation('Player/pacman_up.png', 1, 4, True)
+        pacman_down  = load_animation('Player/pacman_down.png', 1, 4, True)
+        pacman_left  = load_animation('Player/pacman_left.png', 1, 4, True)
+        pacman_right = load_animation('Player/pacman_right.png', 1, 4, True)
         super(Player, self).__init__('Player/pacman_start.png', x, y)
         self.do(actions)
